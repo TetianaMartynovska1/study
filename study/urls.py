924a84fb1eb2fad622aa94ead51588299714a5b3
TetianaMartynovska1 <tetiana.martynovska1@gmail.com>
2024-11-09 18:51:21 +0200

    add form and other method

--- study/urls.py
+++ study/urls.py
@@ -1,14 +1,12 @@
-# from django.conf.urls import url
-# from . import views
-# urlpatterns = [
-#     url(r'^$', views.post_list, name='post_list'),
-# ]
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.blog_home, name='blog_home'),
+    re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
 ]
